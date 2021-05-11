6b876dd7ff08788c69d39675b1814b2695483f68
gitluin <kyluin@outlook.com>
2021-05-11 12:50:04 -0400

    using wal

--- config.h
+++ config.h
@@ -5,11 +5,10 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Source Code Pro:pixelsize=14:antialias=true:autohint=true";
+//static char *font = "Source Code Pro:pixelsize=14:antialias=true:autohint=true";
+static char *font = "Iosevka Custom:pixelsize=14:antialias=true:autohint=true";
 /* Spare fonts */
-//static char *font2[] = { "Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true", };
 static char *font2[] = { "Material Icons:pixelsize=12:antialias=true:autohint=true" };
-/*	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true", };*/
 
 static int borderpx = 0;
 
@@ -88,61 +87,64 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 1;
+float alpha = 0.85;
+
+#include "/home/kyluin/.cache/wal/colors-wal-st.h"
 
 /* Terminal colors (16 first used in escape sequence) */
-static const char *colorname[] = {
-	/* 8 normal colors */
-//	"black",
-//	"red3",
-//	"green3",
-//	"yellow3",
-//	"blue2",
-//	"magenta3",
-//	"cyan3",
-//	"gray90",
+//static const char *colorname[] = {
+//	/* 8 normal colors */
+////	"black",
+////	"red3",
+////	"green3",
+////	"yellow3",
+////	"blue2",
+////	"magenta3",
+////	"cyan3",
+////	"gray90",
+////
+////	/* 8 bright colors */
+////	"gray50",
+////	"red",
+////	"green",
+////	"yellow",
+////	"#5c5cff",
+////	"magenta",
+////	"cyan",
+////	"white",
+////
+////	[255] = 0,
+////
+////	/* more colors can be added after 255 to use with DefaultXX */
+////	"#cccccc",
+////	"#555555",
+////	"black",
+//
+//	/* Normal theme! */
+//	/* 8 normal colors */
+//	[0] = "#000000", /* black   */
+//	[1] = "#ff5555", /* red     */
+//	[2] = "#50fa7b", /* green   */
+//	[3] = "#f1fa8c", /* yellow  */
+//	[4] = "#bd93f9", /* blue    */
+//	[5] = "#ff79c6", /* magenta */
+//	[6] = "#8be9fd", /* cyan    */
+//	[7] = "#bbbbbb", /* white   */
 //
 //	/* 8 bright colors */
-//	"gray50",
-//	"red",
-//	"green",
-//	"yellow",
-//	"#5c5cff",
-//	"magenta",
-//	"cyan",
-//	"white",
+//	[8]  = "#44475a", /* black   */
+//	[9]  = "#ff5555", /* red     */
+//	[10] = "#50fa7b", /* green   */
+//	[11] = "#f1fa8c", /* yellow  */
+//	[12] = "#bd93f9", /* blue    */
+//	[13] = "#ff79c6", /* magenta */
+//	[14] = "#8be9fd", /* cyan    */
+//	[15] = "#ffffff", /* white   */
 //
-//	[255] = 0,
-//
-//	/* more colors can be added after 255 to use with DefaultXX */
-//	"#cccccc",
-//	"#555555",
-//	"black",
-
-	/* Normal theme! */
-	/* 8 normal colors */
-	[0] = "#000000", /* black   */
-	[1] = "#ff5555", /* red     */
-	[2] = "#50fa7b", /* green   */
-	[3] = "#f1fa8c", /* yellow  */
-	[4] = "#bd93f9", /* blue    */
-	[5] = "#ff79c6", /* magenta */
-	[6] = "#8be9fd", /* cyan    */
-	[7] = "#bbbbbb", /* white   */
-
-	/* 8 bright colors */
-	[8]  = "#44475a", /* black   */
-	[9]  = "#ff5555", /* red     */
-	[10] = "#50fa7b", /* green   */
-	[11] = "#f1fa8c", /* yellow  */
-	[12] = "#bd93f9", /* blue    */
-	[13] = "#ff79c6", /* magenta */
-	[14] = "#8be9fd", /* cyan    */
-	[15] = "#ffffff", /* white   */
-
-	/* special colors */
-	[256] = "#282a36", /* background */
-	[257] = "#f8f8f2", /* foreground */
+//	/* special colors */
+//	[256] = "#000000", /* background */
+//	//[256] = "#282a36", /* background */
+//	[257] = "#f8f8f2", /* foreground */
 
 	/* Winter theme! */
 	/* 8 normal colors */
@@ -167,21 +169,21 @@ static const char *colorname[] = {
 //
 //	/* special colors */
 //	[256] = "#000000", /* background */
-//	[257] = "#ffffff", /* foreground */
-
-};
-
-
-/*
- * Default colors (colorname index)
- * foreground, background, cursor, // reverse cursor
- */
-
-/* Normal theme! */
-unsigned int defaultfg = 257;
-unsigned int defaultbg = 256;
-static unsigned int defaultcs = 257;
-static unsigned int defaultrcs = 257;
+////	[257] = "#ffffff", /* foreground */
+//
+//};
+//
+//
+///*
+// * Default colors (colorname index)
+// * foreground, background, cursor, // reverse cursor
+// */
+//
+///* Normal theme! */
+//unsigned int defaultfg = 257;
+//unsigned int defaultbg = 256;
+//static unsigned int defaultcs = 257;
+//static unsigned int defaultrcs = 257;
 
 
 /* Winter theme! */
